15872ed5b978e31dd6a1340e68b53bbf75a1fd49
Kring Villanueva <ksvillanueva7@gmail.com>
2019-11-23 18:48:34 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,6 +1,6 @@
-def greet #method signature
-  puts "Hello name" #method body
+def greet(name) #method signature
+  puts "Hello #{name}" #method body
 end #method closing
 
 #greeting #executing the method by name
-greet
+greet("Sponge")
